925095048694012ea4962944a4a9581166752f48
masonyurko <mjy5264@psu.edu>
2023-03-21 12:24:51 -0400

    First commit

---
+++ stories/badges-app.stories.js
@@ -0,0 +1,25 @@
+import { html } from 'lit';
+import '../src/badges-app.js';
+
+export default {
+  title: 'BadgesApp',
+  component: 'badges-app',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+function Template({ title, backgroundColor }) {
+  return html`
+    <badges-app
+      style="--badges-app-background-color: ${backgroundColor || 'white'}"
+      .title=${title}
+    >
+    </badges-app>
+  `;
+}
+
+export const App = Template.bind({});
+App.args = {
+  title: 'My app',
+};
