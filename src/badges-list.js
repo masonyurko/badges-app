4e5d002574482d523cd1213a18d8cc4ca34f7fe8
masonyurko <mjy5264@psu.edu>
2023-04-05 22:45:35 -0400

    ..

--- src/badges-list.js
+++ src/badges-list.js
@@ -69,6 +69,7 @@ export class BadgesList extends LitElement {
       .bot-block {
         color: black;
         padding: 12px 20px 15px 20px;
+        margin: 50px;
         background-color: white;
         display: inline-block;
       }
