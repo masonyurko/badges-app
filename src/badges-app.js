872a5291afc988cb40f958c468b3763cfe1b9bd1
masonyurko <mjy5264@psu.edu>
2023-04-06 12:26:14 -0400

    merge

--- src/badges-app.js
+++ src/badges-app.js
@@ -6,6 +6,11 @@ class BadgesApp extends LitElement {
   static get properties() {
     return {
       badges: { type: Array },
+      searchBoxDesc: { type: String },
+      badgesDesc: { type: String },
+      explore: { type: String },
+      input: { type: String },
+      items: { type: Array },
     };
   }
 
@@ -13,6 +18,16 @@ class BadgesApp extends LitElement {
     super();
     this.badges = [];
     this.updateBadge();
+    this.searchBoxDesc =
+      'Explore our content in a self guided manner. Want us to guide you through learning new skills? Try out Missions. Looking for other people with similar focus? Find them in Groups. Interested in viewing all the options within a certain subject area? You can do that with Topics.';
+    this.badgesDesc =
+      'Looking for something brand spankin new? Here are the most recently added badges!';
+    this.explore = 'Explore';
+    this.placeholder = 'Search Content, Topics and People';
+  }
+
+  handleInput(e) {
+    this.input = e.target.value;
   }
 
   updateBadge() {
@@ -30,6 +45,56 @@ class BadgesApp extends LitElement {
   }
 
   static styles = css`
+    .app-container {
+      padding-top: 50px;
+      background-color: lightgray;
+      padding-bottom: 500px;
+    }
+    .explore {
+      font-size: 36px;
+      font-family: sans-serif;
+      padding-left: 185px;
+    }
+    .search-desc {
+      font-size: 25px;
+      padding-bottom: 15px;
+    }
+    .search-container {
+      margin: 10px;
+      margin-right: 220px;
+      margin-left: 185px;
+      padding-top: 20px;
+      padding-left: 10px;
+      padding-right: 10px;
+      padding-bottom: 10px;
+
+      background-color: white;
+      font-family: sans-serif;
+    }
+
+    input {
+      padding: 5px;
+      display: inline-block;
+      width: 800px;
+    }
+
+    .badges-desc {
+      font-size: 20px;
+      font-weight: bold;
+      padding-bottom: 20px;
+    }
+    .badges-container {
+      margin: 10px;
+      padding-top: 10px;
+      padding-right: 10px;
+      padding-left: 10px;
+      padding-bottom: 200px;
+      margin-left: 185px;
+      margin-right: 220px;
+      background-color: white;
+      font-family: sans-serif;
+    }
+
     .results {
       display: flex;
       flex-direction: row;
@@ -45,10 +110,27 @@ class BadgesApp extends LitElement {
 
   render() {
     return html`
-    <div class="container"></div>
-        <div class="results">
-          ${this.badges.map(
-            badge => html`
+      <div class="container"></div>
+      <div class="app-container">
+        <label class="explore">${this.explore}</label>
+        <div class="search-container">
+          <div class="search-desc">${this.searchBoxDesc}</div>
+          <div class="search-bar">
+            <simple-icon accent-color="black" icon="search"> </simple-icon>
+            <input
+              type="text"
+              id="getme"
+              @input="${this.handleInput}"
+              placeholder="${this.placeholder}"
+            />
+          </div>
+        </div>
+
+        <div class="badges-container">
+          <label class="badges-desc">${this.badgesDesc}</label>
+          <div class="results">
+            ${this.badges.map(
+              badge => html`
               <badges-list
                 badgeTitle="${badge.badgeTitle}"
                 badgeDesc="${badge.badgeDesc}"
@@ -57,8 +139,13 @@ class BadgesApp extends LitElement {
                 color="${badge.color}"
               >
               </badges-list>
+              </div>
+            <div>
+          <div class="input">${this.input}</div>
+        </div>
             `
-          )}
+            )}
+          </div>
         </div>
       </div>
     `;
