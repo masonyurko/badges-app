a3142d8ce39e15527030062ab5a6be6e3f892115
masonyurko <mjy5264@psu.edu>
2023-04-09 16:53:57 -0400

    good?

--- src/search-bar.js
+++ src/search-bar.js
@@ -24,7 +24,7 @@ export class SearchBar extends LitElement {
 
   constructor() {
     super();
-    this.value = 'Default Value';
+    this.value = 'Search Content, Topics, and People';
   }
 
   render() {
