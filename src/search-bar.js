be97b5343ddd50845c7520908f89d38373c75850
masonyurko <mjy5264@psu.edu>
2023-04-09 17:41:56 -0400

    cc

--- src/search-bar.js
+++ src/search-bar.js
@@ -18,6 +18,20 @@ export class SearchBar extends LitElement {
         display: inline-block;
         --simple-icon-height: 40px;
         --simple-icon-width: 40px;
+        padding-right: 5px;
+      }
+
+      #searchBar {
+        width: 100%;
+        box-sizing: border-box;
+        border: 1px solid #c8c8c8;
+        font-size: 16px;
+        background-color: white;
+        padding: 12px 20px 12px 40px;
+        box-shadow: 10px 5px 5px rgba(0, 0, 0, 0.35);
+        background: url('https://img.icons8.com/fluency-systems-regular/512/search--v1.png')
+          no-repeat left;
+        background-size: 22px;
       }
     `;
   }
